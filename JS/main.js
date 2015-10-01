a2930bf2f2d5aa6f21367cfd660c714e192f443e
Amit Shah <ashah325i@gmail.com>
2015-10-01 17:05:05 -0400

    part of question #2

--- JS/main.js
+++ JS/main.js
@@ -1,37 +1,65 @@
-(function () {
+//(function () {
 
 
 //items.forEach( function (item) {
-// console.log)(item.price);
+// console.log(item.price);
 
-var prices = items.map ( function (item) {
-  return item.price;
+//var prices = items.map ( function (item) {
+  //return item.price;
 
-});
-console.log(prices);
+//});
+//console.log(prices);
 
-var sum = prices.reduce( function (prev, next) {
-  return prev + next
+//var sum = prices.reduce( function (prev, next) {
+  //return prev + next
 
-});
-console.log(sum);
+//});
+//console.log(sum);
 
-var avg = sum / items.length;
-console.log(avg);
+//var avg = sum / items.length;
+//console.log(avg);
 
 // Convert it to 2 decimal places
-var converted = avg.toFixed(2);
-console.log(converted);
+//var converted = avg.toFixed(2);
+//console.log(converted);
 
 // Make it a String
-var str = 'The average price is $' + converted;
-console.log(str);
+//var str = 'The average price is $' + converted;
+//console.log(str);
 
 // Make it show up on the page
-var answer1 = document.querySelector('#answer1');
-var textNode = document.createTextNode(str);
+//var answer1 = document.querySelector('#answer1');
+//var textNode = document.createTextNode(str);
 
-answer1.appendChild(textNode);
+//answer1.appendChild(textNode);
 
 
- }());
+ //}());
+
+
+items.forEach(function(item) {
+  console.log(item.price);
+
+var prices = items.map( function (item) {
+  return item.price;
+
+})
+});
+
+var merchandise = items.filter( function (beers) {
+  if (beers.price > 14 && beers.price < 18){
+  console.log( beers.title)
+
+
+// var answer2 = document.querySelector('#answer2');
+// var textNode = document.createTextNode(str);
+
+// answer2.appendChild(textNode);
+
+ };
+ });
+
+
+
+
+
